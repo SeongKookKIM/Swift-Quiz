10d378b75675666c9e3548cb29fed589aaf9b6b6
SeongKookKIM <ksk8646@naver.com>
2024-04-15 15:13:09 +0900

    Initial Commit

---
+++ Quiz-project/Quiz_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_projectApp.swift
+//  Quiz-project
+//
+//  Created by SeongKook on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
